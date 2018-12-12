e021a76080e15ecc120c42195d7d6c5bf6e10854
张红涛 <zhang.hongtao@zs-hospital.sh.cn>
2018-12-12 18:40:32 +0800

    1

--- config/config.default.js
+++ config/config.default.js
@@ -6,5 +6,28 @@
  * @property {String} SOME_KEY - some description
  */
 exports.mssql2webapi = {
-
+  urlprefix: '/api/',
+  clients: {
+    emr: {// api/emr/
+      db: {
+        server: 'db1',
+        user: 'sa',
+        password: 'test.123456',
+        database: 'db1',
+      },
+      apis: [{
+        name: 'getuser',
+        sql: 'api_getuser',
+        type: 'pro',
+      }, {
+        name: 'findusers',
+        sql: 'select * from users where objectstatus>0',
+        type: 'sql',
+      }],
+    },
+    ris: {
+      db: {},
+      apis: [],
+    },
+  },
 };
